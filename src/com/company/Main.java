710a46b376688521722eb27f863ced5ef7c5948c
Rosi <rosica.stoyanova@mail.bg>
2022-02-22 22:07:05 +0200

    HomeWork1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello my name is Rositsa!");
+
+        String name = "Rositsa";
+        int number = 5;
+        double price = 2.7;
+        Boolean var = true;
+
+        System.out.println("name " + name);
+        System.out.println("number " + number);
+        System.out.println("price " + price);
+        System.out.println("var " + var);
+
     }
 }
