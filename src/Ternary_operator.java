e9a22e63a30ce56a8954b03d872d32d413d5a855
RositsaStoyanova <100231235+RositsaStoyanova@users.noreply.github.com>
2022-03-24 17:13:07 +0200

    Delete src directory

--- src/Ternary_operator.java
+++
@@ -1,9 +0,0 @@
-import java.util.concurrent.ThreadLocalRandom;
-
-public class Ternary_operator {
-    public static void main(String[] args) {
-        int randomNum = ThreadLocalRandom.current().nextInt(-100, 100 + 1);
-        String result = (randomNum > 0) ? "Number is positive." : "Number is negative.";
-        System.out.println(result);
-    }
-}
--- src/homework_task_abs.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class homework_task_abs {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int Number = Integer.parseInt(scanner.nextLine());
-        System.out.println(("output:" + Math.abs(Number)));
-    }
-}
